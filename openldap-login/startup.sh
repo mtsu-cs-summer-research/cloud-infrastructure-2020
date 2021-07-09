2f36106a816ccd0fcc61224f2976aaa3d3b45d72
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2021-07-09 10:31:16 -0500

    Docker image for worker/scheduler nodes

---
+++ openldap-login/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+service xinetd start
+service nscd start
+service ssh start
+
+# Regenerate SSH host key
+/bin/rm -v /etc/ssh/ssh_host_*
+echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
+RUNLEVEL=1 dpkg-reconfigure -f noninteractive openssh-server
+
+if [ -f /etc/munge/munge.key.tmp ]; then
+  echo "Configuring node for mpi cluster: `hostname`"
+  cp /etc/munge/munge.key.tmp /etc/munge/munge.key
+  chown munge:munge /etc/munge/munge.key
+
+  service munge start
+
+  if [ `hostname` != 'server' ]; then
+    echo "Node type: agent"
+    service slurmd start
+  else
+    echo "Node type: server"
+    service slurmctld start
+  fi
+fi
+
+# Just wait
+sleep infinity
+
