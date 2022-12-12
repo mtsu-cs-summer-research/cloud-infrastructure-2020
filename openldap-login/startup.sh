bfeca156a0f0bcc2056f70ac3d2760539f51476d
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2022-12-12 09:24:51 -0600

    Moving to AzureAD in development branch - may rename later

--- openldap-login/startup.sh
+++ openldap-login/startup.sh
@@ -5,30 +5,28 @@
 
 # Regenerate SSH host key
 /bin/rm -v /etc/ssh/ssh_host_*
-echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
-RUNLEVEL=1 dpkg-reconfigure -f noninteractive openssh-server
+# echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
+dpkg-reconfigure -f noninteractive openssh-server
 chmod 600 /etc/ssh/*_key
-
-service xinetd start
-service nscd start
-service ssh start
 
 if [ -f /etc/munge/munge.key.tmp ]; then
   echo "Configuring node for mpi cluster: `hostname`"
   cp /etc/munge/munge.key.tmp /etc/munge/munge.key
   chown munge:munge /etc/munge/munge.key
 
-  service munge start
+  systemctl enable munge
 
   if [ `hostname` != 'scheduler' ]; then
     echo "Node type: agent"
-    service slurmd start
+    systemctl disable slurmctld
+    systemctl enable slurmd
   else
     echo "Node type: scheduler"
-    service slurmctld start
+    systemctl enable slurmctld
+    systemctl disable slurmd
   fi
 fi
 
 # Just wait
-sleep infinity
+exec /sbin/init
 
